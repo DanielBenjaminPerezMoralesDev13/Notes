9e03c8034ef339f9fadc8464b4dc76cd2140ee7f
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 15:14:26 -0600

    * ./Personalization/Vscode/src/helpers/add.js added to repository

---
+++ Personalization/Vscode/src/helpers/add.js
@@ -0,0 +1,16 @@
+// Autor: Daniel Benjamin Perez Morales
+// GitHub: [DanielBenjaminPerezMoralesDev13](https://github.com/DanielBenjaminPerezMoralesDev13)
+// Correo electrónico: danielperezdev@proton.me
+
+/**
+ * Función: **Add 2 numbers together**
+ * 
+ * @param {number} a El primer número a sumar.
+ * @param {number} b El segundo número a sumar.
+ * @returns La suma de `a` y `b`.
+ */
+export default function add(a, b) {
+    return a + b;
+}
+
+// Para importar, es necesario utilizar `export default`, como se muestra aquí.
