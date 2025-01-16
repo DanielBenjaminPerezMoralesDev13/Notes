16c2488960c24dba4f460edd633ec723473c26c1
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 15:13:43 -0600

    * ./Python/Programacion Orientado Objetos POO/02 Conceptos Avanzados/Class/04 Decoradores.py added to repository

---
+++ Python/Programacion Orientado Objetos POO/02 Conceptos Avanzados/Class/04 Decoradores.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Autor: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/DanielBenjaminPerezMoralesDev13
+# Gitlab: https://gitlab.com/DanielBenjaminPerezMoralesDev13
+# Correo electrónico: danielperezdev@proton.me
+
+from sys import stdout
+from typing import Callable
+
+def decorador(funcion: Callable[[],None]) -> Callable[[],None]:
+    def funcion_modificada() -> None:
+        print("Antes de ejecutar la funcion", end = "\n", file = stdout)
+        funcion()
+        print("Despues de ejecutar la funcion", end = "\n", file = stdout)
+        return None
+    return funcion_modificada
+
+
+def saludo() -> None:
+    print("Hola Daniel", end = "\n", file = stdout)
+    return None
+
+saludoModificado: Callable[[],None] = decorador(funcion=saludo)
+saludoModificado()
+
+# Manera mas facil de entender los decoradores pero no es la mas optima
