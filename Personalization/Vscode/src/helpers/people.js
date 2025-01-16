4cfe0817ff6ab8d7df154b2cfff4fd0a9a16adac
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 15:14:26 -0600

    * ./Personalization/Vscode/src/helpers/people.js added to repository

---
+++ Personalization/Vscode/src/helpers/people.js
@@ -0,0 +1,14 @@
+// Autor: Daniel Benjamin Perez Morales
+// GitHub: [DanielBenjaminPerezMoralesDev13](https://github.com/DanielBenjaminPerezMoralesDev13)
+// Correo electrónico: danielperezdev@proton.me
+
+/**
+ * Array de personas.
+ *
+ * @type {Array<Object>} Un array que contiene objetos con nombres de personas.
+ */
+const people = [
+    { name: "Daniel" }
+];
+
+export default people;
