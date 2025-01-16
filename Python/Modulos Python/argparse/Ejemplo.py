1c6722a483e8e7a845a1367ea446d1a11a682144
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 15:14:04 -0600

    * ./Python/Modulos Python/argparse/Ejemplo.py added to repository

---
+++ Python/Modulos Python/argparse/Ejemplo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Author: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/DanielBenjaminPerezMoralesDev13
+# Gitlab: https://gitlab.com/DanielBenjaminPerezMoralesDev13
+# Email: danielperezdev@proton.me
+
+from sys import exit
+from argparse import ArgumentParser
+
+parser: ArgumentParser = ArgumentParser(
+    prog = "Mi_Programa",
+    description = "Este es un programa de ejemplo.",
+    epilog = "Texto adicional al final de la ayuda.",
+    exit_on_error = True,
+    add_help = True
+)
+
+exit(0)
