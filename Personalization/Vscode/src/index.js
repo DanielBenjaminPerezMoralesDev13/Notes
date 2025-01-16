14c55d86c4dc3eff164e50cd0987f3b5739495a2
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 15:14:25 -0600

    * ./Personalization/Vscode/src/index.js added to repository

---
+++ Personalization/Vscode/src/index.js
@@ -0,0 +1,29 @@
+// Autor: Daniel Benjamin Perez Morales
+// GitHub: [DanielBenjaminPerezMoralesDev13](https://github.com/DanielBenjaminPerezMoralesDev13)
+// Correo electrónico: danielperezdev@proton.me
+
+import add from "./helpers/add";
+import people from "./helpers/people";
+
+/**
+ * Función: **Say a message**
+ * 
+ * ```javascript
+ * console.log("xd")
+ * ```
+ * 
+ * @param {string} message Mensaje a mostrar en el cuadro de alerta.
+ * @return {void}
+ */
+function say(message) {
+    alert(message);
+}
+
+// Al hacer clic en la función, se pueden configurar varias opciones.
+// Activar las referencias del icono:
+say("hello");
+
+add(10, 15);
+people;
+
+// Para usar "Go to Symbol", presiona `Ctrl + Shift + P`, borra el `>`, y escribe `#<funcion>` o `@`.
