8a2499292bcd32b03f5546e627b02eac40ad0170
d4nitrix13 <danielperezdev@proton.me>
2025-01-16 14:13:08 -0600

    * ./C/Guia/07 Variable Static/static.c added to repository

---
+++ C/Guia/07 Variable Static/static.c
@@ -0,0 +1,7 @@
+// Autor: Daniel Benjamin Perez Morales
+// GitHub: https://github.com/DanielBenjaminPerezMoralesDev13
+// Correo electrónico: danielperezdev@proton.me
+// static.c
+
+// Definición de la variable static
+int variableStatic = 42;
